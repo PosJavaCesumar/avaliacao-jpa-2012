3d88693d32083c38dd0bafaf2ad58c368243ec5d
Falci <falci@falci.me>
2012-11-10 16:31:01 -0200

    Avaliação em 2012-11-10

--- src/main/java/entidade/Pedido.java
+++ src/main/java/entidade/Pedido.java
@@ -3,22 +3,27 @@ package entidade;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 /**
  * @author Falci
  */
 @Entity
-@DiscriminatorValue("C")
-public class ProdutoComposto extends Produto implements Serializable{
+public class Pedido implements Serializable{
     
-    @OneToMany(mappedBy="produtoComposto")
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    @ManyToMany
     private Set<Produto> produtos = new HashSet<Produto>();
     
     public Set getProdutos(){
         return produtos;
     }
-            
+    
+    
 }
--- src/main/java/entidade/ProdutoSimples.java
+++ src/main/java/entidade/ProdutoSimples.java
@@ -1,13 +1,25 @@
 package entidade;
 
-import javax.persistence.Column;
+import java.io.Serializable;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 import org.hibernate.annotations.Type;
 
 /**
  * @author Falci
  */
-public class ProdutoSimples extends Produto{
+@Entity
+@DiscriminatorValue("S")
+public class ProdutoSimples extends Produto implements Serializable{
 
     @Type(type="text")
     private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
